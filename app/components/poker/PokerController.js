d634bcaff99a50dfef1a6c41c26d7a5f13a54591
Kenneth Bigler <kennethbigler@gmail.com>
2015-12-01 17:30:14 -0800

    updated view and controller for poker

--- app/components/poker/PokerController.js
+++ app/components/poker/PokerController.js
@@ -2,7 +2,10 @@
 
 app.controller('PokerController', function ($scope, $deck) {
     "use strict";
+    
+    /******************************     Prep Data and Variables     ******************************/
     var hand, i, rankSort, suitSort;
+    $scope.trash = [];
     rankSort = function (a, b) {return a.rank - b.rank; };
     suitSort = function (a, b) {
         var ta, tb;
@@ -43,27 +46,17 @@ app.controller('PokerController', function ($scope, $deck) {
         return ta - tb;
     };
     
-    $deck.shuffle();
-    hand = $deck.deal(5);
-    $scope.hand = hand;
-    $scope.hand.sort(suitSort);
-    
-    for (i = 0; i < $scope.hand.length; i += 1) {
-        console.log($scope.hand[i].name);
-    }
-    
+    /******************************     Prep Helper Functions     ******************************/
     /* Pass in an array of index numbers
      * iterate through array, removing each index number from hand
      * add new cards to the hand
     */
-    $scope.discard = function (cards) {
+    function discard(cards) {
         var i;
         for (i = 0; i < cards.length; i += 1) {
-            //splice: (index where, how many to remove)
-            //$scope.hand.splice(cards[i], 1);
             $scope.hand[cards[i]] = $deck.deal(1)[0];
         }
-    };
+    }
     
     /* Compare hands to see who wins
      * Hands should be assigned 3 hex values:
@@ -80,7 +73,7 @@ app.controller('PokerController', function ($scope, $deck) {
      *   1 Pair          1
      *   High Card       0
     */
-    $scope.evaluate = function (hand) {
+    function evaluate(hand) {
         var i, j, hist, temp, s, f;
         // Histogram for the cards
         hist = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -94,28 +87,34 @@ app.controller('PokerController', function ($scope, $deck) {
         // 4 of a kind
         if (temp !== -1) {
             console.log(temp);
-            // for texas holdem, need to return indexOf(1) instead of 0 #1
-            // return 7 / temp /0/0/0/0
+            return "7" + temp.toString(13) + "0000";
+            // for texas holdem, need to return indexOf(1) instead of first 0
         }
         temp = hist.indexOf(3);
         i = hist.indexOf(2);
         j = hist.indexOf(2, i + 1);
         if (temp !== -1 && i !== -1) {
             // full house
-            // return 6 / temp / i /0/0/0
+            return "6" + temp.toString(13) + i.toString(13) + "000";
         } else if (temp !== -1) {
             // 3 of a kind
-            // return 3 / temp / lastIndexOf(1) / indexOf(1) /0/0
+            i = hist.lastIndexOf(1).toString(13);
+            j = hist.indexOf(1).toString(13);
+            return "3" + temp.toString(13) + i + j + "00";
         } else if (i !== -1 && j !== -1) {
             // 2 pair
-            // return 2 / j / i / indexOf(1) /0/0
+            temp = hist.indexOf(1).toString(13);
+            return "2" + j.toString(13) + i.toString(13) + temp + "00";
         } else if (i !== -1) {
             // 1 pair
-            // return 1 / i / lastIndexOf(1) / lastIndexOf(1, q) / indexOf(1) /0
+            j = hist.lastIndexOf(1);
+            temp = hist.lastIndexOf(1, j - 1).toString(13);
+            s = hist.indexOf(1).toString(13);
+            return "1" + i.toString(13) + j.toString(13) + temp + s + "0";
         } else {
             // all single cards, look for flush and straight
             temp = hist.indexOf(1);
-            j = hist.lsatIndexOf(1);
+            j = hist.lastIndexOf(1);
             // check for straight
             s = ((j - temp) === 4);
             // A,2,3,4,5
@@ -133,32 +132,68 @@ app.controller('PokerController', function ($scope, $deck) {
                 }
             }
             if (s && f) {
-                // return 8 / j /0/0/0/0
+                return "8" + j.toString(13) + "0000";
             } else if (f) {
-                // 2 of 5 
-                // return 5 / j / hist.indexOf(1, j-1) / hist.indexOf(1, k-1) / hist.indexOf(1, L-1) / hist.indexOf(1, m-1)
+                // flush
+                i = hist.lastIndexOf(1, j - 1);
+                temp = hist.lastIndexOf(1, i - 1);
+                s = hist.lastIndexOf(1, temp - 1);
+                f = hist.lastIndexOf(1, s - 1);
+                return "5" + j.toString(13) + i.toString(13) + temp.toString(13) + s.toString(13) + f.toString(13);
             } else if (s) {
-                // return 4 / j /0/0/0/0
+                // straight
+                return "4" + j.toString(13) + "0000";
             } else {
-                // 2 of 5
-                // return 0 / j / hist.indexOf(1, j-1) / hist.indexOf(1, k-1) / hist.indexOf(1, L-1) / hist.indexOf(1, m-1)
+                // high card
+                i = hist.lastIndexOf(1, j - 1);
+                temp = hist.lastIndexOf(1, i - 1);
+                s = hist.lastIndexOf(1, temp - 1);
+                f = hist.lastIndexOf(1, s - 1);
+                return "0" + j.toString(13) + i.toString(13) + temp.toString(13) + s.toString(13) + f.toString(13);
             }
         }
+    }
+    
+    function print(hand) {
+        $scope.hand.sort(rankSort);
+        for (i = 0; i < hand.length; i += 1) {
+            console.log(hand[i].name);
+        }
+    }
+    
+    /******************************     Prep View Functions     ******************************/
+    // call the evaluate function for each hand and determine winner
+    $scope.evaluate = function () {
+        // to determine winner, decode w/ z = parseInt(result, 13);
+        var r = evaluate($scope.hand);
+        console.log(r);
+        console.log(parseInt(r, 13));
+    };
+    
+    // select cards to discard
+    $scope.toss = function (t) {
+        var i = $scope.trash.indexOf(t);
+        if (i !== -1) {
+            //splice: (index where, how many to remove)
+            $scope.hand.splice(i, 1);
+        } else {
+            $scope.trash.push(t);
+        }
+        console.log($scope.trash);
+    };
+    
+    // discard selected cards and get replacements
+    $scope.discard = function () {
+        discard($scope.trash);
+        $scope.trash = [];
     };
     
     
-    // testing
-    $scope.discard([1, 3, 4]);
+    
+    /******************************     Testing     ******************************/
+    $deck.shuffle();
+    hand = $deck.deal(5);
+    $scope.hand = hand;
     $scope.hand.sort(suitSort);
-    console.log($scope.hand);
-    for (i = 0; i < $scope.hand.length; i += 1) {
-        console.log($scope.hand[i].name);
-    }
+    print($scope.hand);
 });
-
-
-
-
-
-
-// js hex representation: 0xFFF;
