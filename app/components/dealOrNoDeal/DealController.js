5629aa58764967c5763aed036e280d00db1f232c
kennethbigler <kennethbigler@gmail.com>
2016-10-14 18:09:59 -0700

    show/dont show deal/no deal

--- app/components/dealOrNoDeal/DealController.js
+++ app/components/dealOrNoDeal/DealController.js
@@ -9,7 +9,9 @@ app.controller('DealController', ['$scope', 'DealService', '$log', function ($sc
     $scope.valClass = [];
     $scope.counter = 0;
     $scope.offer = 0;
+    $scope.showOffer = false;
     
+    // reset game variables to start fresh
     $scope.newGame = function () {
         $DS.newGame();
         for (i = 0; i < board.length; i += 1) {
@@ -21,13 +23,14 @@ app.controller('DealController', ['$scope', 'DealService', '$log', function ($sc
         sum = 3418418;
         cases = 26;
         $scope.counter = 7;
-        $scope.offer = (sum / cases) * (turn / 10);
-        
+        $scope.showOffer = false;
         return;
     };
     $scope.newGame();
     
+    // called each time a case is selected
     $scope.caseSelect = function (x) {
+        // if clicking a case w/ no cases to open left or first click
         if ($scope.counter === 0) {
             return;
         } else if ($scope.counter === 7) {
@@ -39,36 +42,42 @@ app.controller('DealController', ['$scope', 'DealService', '$log', function ($sc
         
         var t, n, vals = [1, 2, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];
         
+        // verify case has not been opened already
         n = $DS.openCase(x);
         if (!n) {
             return;
         }
         
+        // change visuals of selected case
         $scope.board[x] = n;
         $scope.caseClass[x] = 'btn-danger';
         t = vals.indexOf(n);
         $scope.valClass[t] = 'btn-danger';
-        $scope.counter -= 1;
+        
+        // update for offer
         sum -= n;
         cases -= 1;
-        $scope.offer = (sum / cases) * (turn / 10);
         
+        // show deal or no deal if last case
+        $scope.counter -= 1;
+        if ($scope.counter === 0) {
+            $scope.offer = (sum / cases) * (turn / 10);
+            $scope.showOffer = true;
+        }
         return;
     };
     
+    // called on selection of Deal
     $scope.deal = function () {
         $scope.newGame();
         return;
     };
     
+    // called on selection of No Deal
     $scope.noDeal = function () {
+        $scope.showOffer = false;
         turn += 1;
-        $scope.offer = (sum / cases) * (turn / 10);
-        if (turn > 6) {
-            $scope.counter = 1;
-        } else {
-            $scope.counter = 7 - turn;
-        }
+        $scope.counter = (turn < 6) ? 7 - turn : 1;
         return;
     };
 }]);
